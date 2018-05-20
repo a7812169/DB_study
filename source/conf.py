eb8acdf4fbc24482c81ed4be53e37a0be1760c31
a7812169 <32430997+a7812169@users.noreply.github.com>
2018-05-20 13:59:23 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -1,3 +1,4 @@
+
 # -*- coding:utf-8 -*-
 # Configuration file for the Sphinx documentation builder.
 #
@@ -14,6 +15,23 @@
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
+latex_elements={# The paper size ('letterpaper' or 'a4paper').
+'papersize':'a4paper',# The font size ('10pt', '11pt' or '12pt').
+'pointsize':'12pt','classoptions':',oneside','babel':'',#必須
+'inputenc':'',#必須
+'utf8extra':'',#必須
+# Additional stuff for the LaTeX preamble.
+'preamble': r"""
+\usepackage{xeCJK}
+\usepackage{indentfirst}
+\setlength{\parindent}{2em}
+\setCJKmainfont{WenQuanYi Micro Hei}
+\setCJKmonofont[Scale=0.9]{WenQuanYi Micro Hei Mono}
+\setCJKfamilyfont{song}{WenQuanYi Micro Hei}
+\setCJKfamilyfont{sf}{WenQuanYi Micro Hei}
+\XeTeXlinebreaklocale "zh"
+\XeTeXlinebreakskip = 0pt plus 1pt
+"""}
 # -- Project information -----------------------------------------------------
 project = 'DB组学习中心'
 copyright = '2018, DP'
